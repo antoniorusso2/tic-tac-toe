ffcc14e446620b3d7fac95c3c83b43bb730f01fe
antoniorusso2 <russoantonio303@outlook.it>
2025-01-20 11:57:35 +0100

    add handle click function for sowing x in every board field

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
+import { useState } from 'react'
 import './App.css'
 import Square from './components/Square'
+import GlobalContext from './globalContext'
 
 function Board() {
+  //variabile che indica il valore di ogni quadrato per stabilire poi un vincitore
+  const [squares, setSquares] = useState(Array(9).fill(null))
+
   return (
-    <div className="container">
-      <div className="row">
-        <div className="board">
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
-          <Square />
+    <GlobalContext.Provider value={{ squares, setSquares }}>
+      <div className="container">
+        <div className="row">
+          <div className="board">
+            {squares.map((square, i) => (
+              <Square key={i} index={i} value={square} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </GlobalContext.Provider>
   )
 }
 
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,25 +1,26 @@
 /* eslint-disable react/prop-types */
-/**
- * Square is a functional component that renders a button with a number.
- * When the button is clicked, it logs the number to the console.
- *
- * @param {Object} props - The properties object.
- * @param {string} props.number - The number to display on the button.
- * @returns {JSX.Element} A button element displaying the number.
- */
+import { useContext } from 'react'
+import GlobalContext from '../globalContext'
 
-import { useState } from 'react'
-
-export default function Square() {
+export default function Square({ value, index }) {
   //variabile che indica il valore di ogni quadrato
-  const [value, setValue] = useState(null)
+  // const [value, setValue] = useState(null)
 
-  function handleClick() {
-    //funzione che cambia il valore di ogni quadrato
-    setValue('X')
+  const { squares, setSquares } = useContext(GlobalContext)
+
+  function handleClick(i) {
+    //copia dell'array di sezioni dove poter inserire il simbolo
+    const nextSquares = squares.slice()
+
+    //imposta il valore del quadrato cliccato
+    nextSquares[i] = 'X'
+
+    //aggiorna lo stato del componente
+    setSquares(nextSquares)
   }
+
   return (
-    <button onClick={handleClick} className="square">
+    <button onClick={() => handleClick(index)} className="square">
       {value}
     </button>
   )
