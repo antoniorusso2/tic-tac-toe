f40902711dfd14f1d02fffbd4ae1f3ca5b8ac833
antoniorusso2 <russoantonio303@outlook.it>
2025-02-03 20:24:55 +0100

    add players turn with different symbols

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import Square from './components/Square'
-import GlobalContext from './globalContext'
+import { useState } from 'react';
+import './App.css';
+import Square from './components/Square';
+import GlobalContext from './globalContext';
 
 function Board() {
   //variabile che indica il valore di ogni quadrato per stabilire poi un vincitore
-  const [squares, setSquares] = useState(Array(9).fill(null))
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
 
   return (
-    <GlobalContext.Provider value={{ squares, setSquares }}>
+    <GlobalContext.Provider
+      value={{ squares, setSquares, xIsNext, setXIsNext }}
+    >
       <div className="container">
         <div className="row">
           <div className="board">
@@ -19,7 +22,7 @@ function Board() {
         </div>
       </div>
     </GlobalContext.Provider>
-  )
+  );
 }
 
-export default Board
+export default Board;
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,27 +1,40 @@
 /* eslint-disable react/prop-types */
-import { useContext } from 'react'
-import GlobalContext from '../globalContext'
+import { useContext } from 'react';
+import GlobalContext from '../globalContext';
 
 export default function Square({ value, index }) {
   //variabile che indica il valore di ogni quadrato
   // const [value, setValue] = useState(null)
 
-  const { squares, setSquares } = useContext(GlobalContext)
+  const { squares, setSquares, xIsNext, setXIsNext } =
+    useContext(GlobalContext);
 
   function handleClick(i) {
+    //gestione players turn
+    setXIsNext(!xIsNext);
+
     //copia dell'array di sezioni dove poter inserire il simbolo
-    const nextSquares = squares.slice()
+    const nextSquares = squares.slice();
+
+    //determina il simbolo da inserire
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+
+    console.log(squares, nextSquares);
 
     //imposta il valore del quadrato cliccato
-    nextSquares[i] = 'X'
+    // nextSquares[i] = 'X';
 
     //aggiorna lo stato del componente
-    setSquares(nextSquares)
+    setSquares(nextSquares);
   }
 
   return (
     <button onClick={() => handleClick(index)} className="square">
       {value}
     </button>
-  )
+  );
 }
